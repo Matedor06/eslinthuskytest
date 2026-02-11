d21d88c2caf7b2e29f8a3e51371f34fa11e36536
Karasz Mate <b1karmat@vasvari.org>
2026-02-11 12:17:37 +0100

    instead of jest we use now Vitest

---
+++ src/App.test.tsx
@@ -0,0 +1,25 @@
+// @vitest-environment happy-dom
+
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import App from './App';
+
+describe('App (real API)', () => {
+    it('igazi API-ból betölti és megjeleníti a Margheritát', async () => {
+        render(<App />);
+
+        // megvárja, amíg a useEffect lefut + API válasz megjön + render
+        const el = await screen.findByText('Margherita', {}, { timeout: 10000 });
+        expect(el).toBeTruthy();
+    });
+});
+
+describe('App (real API)', () => {
+    it('random szoveg', async () => {
+        render(<App />);
+
+        // megvárja, amíg a useEffect lefut + API válasz megjön + render
+        const el = await screen.findByText('Tarjás', {}, { timeout: 10000 });
+        expect(el).toBeTruthy();
+    });
+});
